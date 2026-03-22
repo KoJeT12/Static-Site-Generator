8024de48ecb2a98ba098df8636c6e79abcd50a73
KoJeT <krubbs12@yahoo.com>
2026-03-22 15:35:01 -0700

    TestCode

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode, TextType
+
+def main():
+    testType = TextType.text_code
+    testUrl = "https://www.boot.dev"
+    testNode = TextNode("Test Text", testType, testUrl)
+    print(testNode)
+
+main()
