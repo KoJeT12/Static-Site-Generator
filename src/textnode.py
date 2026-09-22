1bfe897946b302e297cabb2ca6a7aa123bd5bb76
KoJeT <krubbs12@yahoo.com>
2026-09-21 18:17:27 -0700

    ah44

--- src/textnode.py
+++ src/textnode.py
@@ -7,7 +7,7 @@ class TextType(Enum):
     CODE = "Code"
     LINK = "Link"
     IMAGE = "Image"
-#ah43
+#ah44
 class TextNode():
     def __init__(self, text, text_type, url=None):
         self.text = text
